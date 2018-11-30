bdc10451b41fbcfae1d983bbd473f84ba530c9cb
joonmosqueda <mosquedajj@yahoo.com>
2018-11-30 22:06:32 +1100

    remove proxy

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-export http_proxy=http://forwardproxy:3128
-export https_proxy=http://forwardproxy:3128
-export no_proxy=localhost,169.254.169.254,patching-server-hui.ext.national.com.au
-export AWS_DEFAULT_REGION=ap-southeast-2
+#export http_proxy=http://forwardproxy:3128
+#export https_proxy=http://forwardproxy:3128
+#export no_proxy=localhost,169.254.169.254,patching-server-hui.ext.national.com.au
+#export AWS_DEFAULT_REGION=ap-southeast-2
 
 BASE_DIR=$(dirname $0)
 
